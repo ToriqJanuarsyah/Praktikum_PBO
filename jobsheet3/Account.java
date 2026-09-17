9ab2f3ba0a9e5f8103aa460bd7cf5525a771fec7
ToriqJanuarsyah <toriqjanuarsyah@gmail.com>
2026-09-18 04:24:59 +0700

    jobsheet3

---
+++ jobsheet3/Account.java
@@ -0,0 +1,48 @@
+package Praktikum_PBO.jobsheet3;
+
+public class Account {
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+    private double dailyWithdrawLimit;
+
+    public Account(String accountNumber, String ownerName, double balance , double dailyWithdrawLimit) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+        this.dailyWithdrawLimit = dailyWithdrawLimit;    
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean deposit(double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        balance += amount;
+        return true;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= 0 || amount > balance || amount > dailyWithdrawLimit) {
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+
+    public void printInfo() {
+        System.out.println(accountNumber + " - " + ownerName + " - balance: " + balance);
+    }
+}
+
